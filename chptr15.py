a7c950876a93e28e1f6cb592c32b23039d5c3bc9
Usman Ammar <usmanammar@MacBook-Pro-4.local>
2021-09-13 00:00:03 +0100

    changes

--- chptr15.py
+++ chptr15.py
@@ -4,6 +4,8 @@
 
 # Craetes table called 'Users' with columns name (text) and email (text)
 # CREATE TABLE "Users" ("name" TEXT, "email" TEXT)
+# VARCHAR(128) is characters up to 128 long
+# CREATE TABLE "Users" ("name" VARCHAR(128), "email" VARCHAR(128))
 
 # Adding information into the TABLE
 # INSERT INTO Users (name, email) VALUES ('Chuck', 'csev@umich.edu')
@@ -22,34 +24,69 @@
 
 # SQL and Python
 
+# import sqlite3
+#
+# # Opening database reqires two steps: connect and cursor
+# # The connect operation makes a “connection” to the database stored in the
+# # file emaildb.sqlite in the current directory.
+# # If the file does not exist, it will be created.
+# conn = sqlite3.connect('emaildb.sqlite')
+# cur = conn.cursor() # Similar to the open command with files
+#
+# # DROP TABLE command deletes the table and all of its contents from the database
+# cur.execute('DROP TABLE IF EXISTS Counts')
+#
+# cur.execute('CREATE TABLE Counts (email TEXT, count INTEGER)')
+#
+# fname = input('Enter file name:')
+# if (len(fname) < 1): fname = 'mbox-short.txt'
+# fh = open(fname)
+# for line in fh:
+#     if not line.startswith('From: '): continue
+#     pieces = line.split()
+#     email = pieces[1]
+#     cur.execute('SELECT count FROM Counts WHERE email = ?', (email,))
+#     # ? is a placeholder which is replaced by the touple (email,)
+#     row = cur.fetchone()
+#     if row is None:
+#         cur.execute(''' INSERT INTO Counts (email, count)
+#         VALUES (?,1)''',(email,))
+#     else:
+#         cur.execute('UPDATE Counts SET count = count + 1 WHERE email = ?',
+#         (email,))
+#     conn.commit()
+#
+# sqlstr = 'SELECT email, count FROM Counts ORDER BY count DESC LIMIT 10'
+#
+# for row in cur.execute(sqlstr):
+#     print(str(row[0]),row[1])
+
+# Assignment
+
 import sqlite3
+import re
 
-# Opening database reqires two steps: connect and cursor
-# The connect operation makes a “connection” to the database stored in the
-# file emaildb.sqlite in the current directory.
-# If the file does not exist, it will be created.
 conn = sqlite3.connect('emaildb.sqlite')
-cur = conn.cursor() # Similar to the open command with files
-
-# DROP TABLE command deletes the table and all of its contents from the database
+cur = conn.cursor()
 cur.execute('DROP TABLE IF EXISTS Counts')
 
-cur.execute('CREATE TABLE Counts (email TEXT, count INTEGER)')
+cur.execute('CREATE TABLE Counts (org TEXT, count INTEGER)')
+
 
 fname = input('Enter file name:')
-if (len(fname) < 1): fname = 'mbox-short.txt'
+if (len(fname) < 1): fname = 'mbox.txt'
 fh = open(fname)
+
 for line in fh:
-    if not line.startswith('From: '): continue
-    pieces = line.split()
-    email = pieces[1]
-    cur.execute('SELECT count FROM Counts WHERE email = ?', (email,))
-    # ? is a placeholder which is replaced by the touple (email,)
+    if not line.startswith('    by'): continue
+    x = re.search('\S+@+([A-z]+)',line)
+    org = x.group(1)
+    cur.execute('SELECT count FROM Counts WHERE org = ?', (org,))
     row = cur.fetchone()
     if row is None:
-        cur.execute(''' INSERT INTO Counts (email, count)
-        VALUES (?,1)''',(email,))
+        cur.execute(''' INSERT INTO Counts (org, count)
+        VALUES (?,1)''',(org,))
     else:
-        cur.execute('UPDATE Counts SET count = count + 1 WHERE email = ?',
-        (email,))
-    conn.commit()
+        cur.execute('UPDATE Counts SET count = count + 1 WHERE org = ?',
+        (org,))
+conn.commit()
