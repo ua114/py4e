a35e9338c57b5ef1b595df4a3769d8b26a71a0cc
Usman Ammar <usmanammar@MacBook-Pro-4.local>
2021-09-15 23:29:36 +0100

    sent

--- chptr15.py
+++ chptr15.py
@@ -63,30 +63,46 @@
 
 # Assignment
 
-import sqlite3
-import re
+# import sqlite3
+# import re
+#
+# conn = sqlite3.connect('emaildb.sqlite')
+# cur = conn.cursor()
+# cur.execute('DROP TABLE IF EXISTS Counts')
+#
+# cur.execute('CREATE TABLE Counts (org TEXT, count INTEGER)')
+#
+#
+# fname = input('Enter file name:')
+# if (len(fname) < 1): fname = 'mbox.txt'
+# fh = open(fname)
+#
+# for line in fh:
+#     if not line.startswith('From: '): continue
+#     x = re.search('\S+@+([A-z]+)',line)
+#     # x = re.search('\S+@+(\S+)',line)
+#     org = x.group(1)
+#     # pieces = line.split('@')
+#     # email = pieces[1]
+#     # org = email
+#     cur.execute('SELECT count FROM Counts WHERE org = ?', (org,))
+#     row = cur.fetchone()
+#     if row is None:
+#         cur.execute(''' INSERT INTO Counts (org, count)
+#         VALUES (?,1)''',(org,))
+#     else:
+#         cur.execute('UPDATE Counts SET count = count + 1 WHERE org = ?',
+#         (org,))
+# conn.commit()
+#
+# sqlstr = 'SELECT org, count FROM Counts ORDER BY count DESC LIMIT 10'
+#
+# for row in cur.execute(sqlstr):
+#     print(str(row[0]),row[1])
 
-conn = sqlite3.connect('emaildb.sqlite')
-cur = conn.cursor()
-cur.execute('DROP TABLE IF EXISTS Counts')
 
-cur.execute('CREATE TABLE Counts (org TEXT, count INTEGER)')
-
-
-fname = input('Enter file name:')
-if (len(fname) < 1): fname = 'mbox.txt'
-fh = open(fname)
-
-for line in fh:
-    if not line.startswith('    by'): continue
-    x = re.search('\S+@+([A-z]+)',line)
-    org = x.group(1)
-    cur.execute('SELECT count FROM Counts WHERE org = ?', (org,))
-    row = cur.fetchone()
-    if row is None:
-        cur.execute(''' INSERT INTO Counts (org, count)
-        VALUES (?,1)''',(org,))
+for integer in (-1,3,5):
+    if integer < 0:
+    	print("negative")
     else:
-        cur.execute('UPDATE Counts SET count = count + 1 WHERE org = ?',
-        (org,))
-conn.commit()
+    	print("non-negative")
